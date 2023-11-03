d3488ea6d9749bc00c50870417b3abf5154904bb
Lucas Handekyn <lucas.handekyn@gmail.com>
2023-11-03 14:44:45 +0100

    cleanup

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from math import *
 import scipy.io as sio
 from numpy import *
 
+
 def channel2APDP(frequentiekarateristiek):
     inv_four = fft.ifft(frequentiekarateristiek)
     return inv_four
@@ -14,16 +15,13 @@ def calculate_delays():
 def main():
     dataset_1 = sio.loadmat("Dataset_1.mat")
     data = dataset_1["H"]
-    punt = data[:,:,1]
+    punt = data[:, :, 1]
     punt = [rij[0] for rij in punt]
     print(punt)
     print("enterezfndxnvnqsdlnvklnqdflknvklqdfnklvn")
     APDP = channel2APDP(punt[:200])
     print(APDP)
     print("Hello World!")
-
-
-
 
 
 if __name__ == "__main__":
